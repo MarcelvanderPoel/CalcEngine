2ecbf18676c87df5f4b9f0305ceb9fe0ce4bae89
Marcel van der Poel <marcel@vanderpoelonline.nl>
2018-01-09 22:50:25 +0100

    update

--- src/com/pluralsight/calcengine/MathEquation.java
+++ src/com/pluralsight/calcengine/MathEquation.java
@@ -40,6 +40,22 @@ public class MathEquation {
     }
 
     // execute
+    public void execute(double leftVal, double rightVal) {
+        this.leftVal = leftVal;
+        this.rightVal = rightVal;
+        execute();
+
+    }
+
+    public void execute(int leftVal, int rightVal) {
+        this.leftVal = leftVal;
+        this.rightVal = rightVal;
+        execute();
+
+        result = (int)result;
+
+    }
+
     public void execute() {
 
         switch(opCode) {
